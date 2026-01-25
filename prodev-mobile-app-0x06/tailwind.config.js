7980ff92bd568ed18c74d6cc959bac4d9ed8eb17
denisktooh <199378976+denisktooh@users.noreply.github.com>
2026-01-25 15:26:27 +0300

    Styling with NativeWind (TailwindCSS)

---
+++ prodev-mobile-app-0x06/tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./app/**/*.{js,jsx,ts,tsx}",
+    "./components/**/*.{js,jsx,ts,tsx}",
+    "./screens/**/*.{js,jsx,ts,tsx}",
+    "./styles/**/*.{js,jsx,ts,tsx,css}"
+  ],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+};
